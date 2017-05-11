625762eb0f22e181b32344cecb03460ad4d22b66
megmatty <meghopweb@me.com>
2017-05-10 19:59:05 -0700

    config repair

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
-		       'mongodb://localhost/moodful';
-                      //'mongodb://megmatty:moodful@ds125481.mlab.com:25481/moodful-data';
+		       // 'mongodb://localhost/moodful';
+                      'mongodb://megmatty:moodful@ds125481.mlab.com:25481/moodful-data';
 
 exports.TEST_DATABASE_URL = (
 	process.env.TEST_DATABASE_URL ||
